0a63e44287417a4a11293ce75aa04bd5a97fe60a
yangyunhe369 <314786482@qq.com>
2017-06-28 14:51:54 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # jQuery-Yys-Slider
-高仿阴阳师官网轮播图效果的jQuery插件
+一个仿阴阳师官网轮播图效果的jQuery插件
+
+该轮播图效果为切换轮播图时利用CSS3D旋转实现的效果。
+
+ps:由于本插件使用了大量CSS3属性，对于低版本浏览器（如ie9以下版本等不支持CSS3属性的浏览器）不兼容。
+
+**demo**：[线上地址](https://yangyunhe369.github.io/jQuery-Yys-Slider/)
+
+## 使用方法
+
+``` javascript
+调用轮播插件：
+Html：
+<div class="content-part part-tese">
+  <div class="content-title"></div>
+  <div class="shadow"></div>
+  <div class="gallery_container">
+    <div class="gallery_wrap threeD_gallery_wrap">
+    	<div href="javascript:;" class="gallery_item threeD_gallery_item gallery_left_middle">
+        <img src="..." class="show">
+        <div class="line-t"></div>
+        <div class="line-r"></div>
+        <div class="line-b"></div>
+        <div class="line-l"></div>
+      </div>
+      <div href="javascript:;" class="gallery_item threeD_gallery_item front_side">
+        <img src="..." class="show">
+        <div class="line-t"></div>
+        <div class="line-r"></div>
+        <div class="line-b"></div>
+        <div class="line-l"></div>
+      </div>
+      <div href="javascript:;" class="gallery_item threeD_gallery_item gallery_right_middle">
+        <img src="..." class="show">
+        <div class="line-t"></div>
+        <div class="line-r"></div>
+        <div class="line-b"></div>
+        <div class="line-l"></div>
+      </div>
+      <div href="javascript:;" class="gallery_item threeD_gallery_item gallery_out">
+        <img src="..." class="show">
+        <div class="line-t"></div>
+        <div class="line-r"></div>
+        <div class="line-b"></div>
+        <div class="line-l"></div>
+      </div>
+      ...
+      (<div href="javascript:;" class="gallery_item threeD_gallery_item gallery_out">
+        <img src="..." class="show">
+        <div class="line-t"></div>
+        <div class="line-r"></div>
+        <div class="line-b"></div>
+        <div class="line-l"></div>
+      </div>) * n
+      <a class="prev" href="javascript:;"></a>
+      <a class="next" href="javascript:;"></a>
+    </div>
+  </div>
+</div>
+//当图片数量大于4张时，需再添加类名为".gallery_out"的容器
+Javascript：
+$(".gallery_container").gallery_slider({
+	imgNum: 4 //轮播图数量至少为4张
+});
+```
+
+## 个人简介
+作者：弦云孤赫(David Yang)
+
+职业：web前端开发工程师
+
+爱好：网游、音乐（吉他）
+
+## 联系方式
+QQ：314786482
+
+微信：yangyunhe_yyh
+
+坐标：四川成都
+
---
+++ js/jquery-yys-slider.js
@@ -0,0 +1,49 @@
+﻿/* by:Yangyunhe——David Yang
+***阴阳师轮播图插件
+***插件支持轮播所需图片数量为至少为四张
+***在调用该插件时传入图片数量
+*/
+(function($) {
+	$.fn.gallery_slider = function(options) {
+	  var _ops = $.extend({
+	      imgNum: 5 , //图片数量
+	      gallery_item_left: '.prev' , //左侧按钮
+	      gallery_item_right: '.next' , //右侧按钮
+	      gallery_left_middle: '.gallery_left_middle', //左侧图片容器
+	      gallery_right_middle: '.gallery_right_middle', //左侧图片容器
+	      threeD_gallery_item: '.threeD_gallery_item' //图片容器
+	  }, options);
+	  var _this = $(this),
+	  		_imgNum = _ops.imgNum, //图片数量
+	  		_gallery_item_left = _ops.gallery_item_left, //左侧按钮
+	  		_gallery_item_right = _ops.gallery_item_right, //右侧按钮
+	  		_gallery_left_middle = _ops.gallery_left_middle, //左侧图片容器
+	  		_gallery_right_middle = _ops.gallery_right_middle, //左侧图片容器
+	  		_threeD_gallery_item = _ops.threeD_gallery_item; //图片容器
+	  		
+  	//左侧按钮绑定点击事件
+  	_this.find(_gallery_item_left).on('click',function(){
+			var idx = parseInt(_this.find(_gallery_left_middle).index());
+			//当前展示图片逻辑
+			_this.find(_threeD_gallery_item).eq(idx).removeClass('gallery_left_middle').addClass('front_side');
+			//当idx值为0时，执行逻辑
+			_this.find(_threeD_gallery_item).eq(idx == 0 ? idx + _imgNum - 1 : idx - 1).removeClass('gallery_out').addClass('gallery_left_middle');
+			//当idx值为_imgNum - 3时，执行逻辑
+			_this.find(_threeD_gallery_item).eq(idx == _imgNum - 3 ? idx + 2 : idx - _imgNum + 2).removeClass('gallery_right_middle').addClass('gallery_out');
+			//当idx值为_imgNum - 2时，执行逻辑
+			_this.find(_threeD_gallery_item).eq(idx == _imgNum - 2 ? idx + 1 : idx - _imgNum + 1).removeClass('front_side').addClass('gallery_right_middle');
+		})
+		//右侧按钮绑定点击事件
+		_this.find(_gallery_item_right).on('click',function(){
+			var idx = parseInt(_this.find(_gallery_right_middle).index());
+			//当前展示图片逻辑
+			_this.find(_threeD_gallery_item).eq(idx).removeClass('gallery_right_middle').addClass('front_side');
+			//当idx值为0时，执行逻辑
+			_this.find(_threeD_gallery_item).eq(idx == 0 ? idx + _imgNum - 1 : idx - 1).removeClass('front_side').addClass('gallery_left_middle');
+			//当idx值为1时，执行逻辑
+			_this.find(_threeD_gallery_item).eq(idx == 1 ? idx + _imgNum - 2 : idx - 2).removeClass('gallery_left_middle').addClass('gallery_out');
+			//当idx值为_imgNum - 2时，执行逻辑
+			_this.find(_threeD_gallery_item).eq(idx == _imgNum - 2 ? idx + 1 : idx - _imgNum + 1).removeClass('gallery_out').addClass('gallery_right_middle');
+		})
+	};
+})(jQuery);
